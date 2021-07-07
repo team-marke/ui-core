40de41d07663d8045e816253687afaf9269aca97
Lucas Land <lucas@marke.com.br>
2021-07-07 17:07:56 -0300

    Adaptado estrutura do SASS usando módulos

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,9 @@
 import VideoTeaser from "./components/video-teaser/video-teaser";
 import VideoModal from "./components/video-modal/video-modal";
+import PhotoGallery from "./components/photo-gallery/photo-gallery";
 
 export {
   VideoTeaser,
   VideoModal,
+  PhotoGallery,
 };
