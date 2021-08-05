f2ae4c7d7b4421fde074a0cf77701d2e65d446a3
marke-franco-nienow <franco@marke.com.br>
2021-08-05 15:19:27 -0300

    Início do navbar

--- components/action-bar/action-bar.js
+++ components/action-bar/action-bar.js
@@ -5,36 +5,19 @@
 import { dom as faDom, library as faLibrary } from '@fortawesome/fontawesome-svg-core';
 import { faLinkedin, faFacebookSquare, faYoutubeSquare, faInstagramSquare } from '@fortawesome/free-brands-svg-icons';
 
-export class ActionBar{
+export class ActionBar {
   /**
    * Inits a new action bar.
    * @param {HTMLElement} el
    */
-  constructor(el){
+  constructor(el) {
     if (el && el instanceof Element) {
-      this.el = el;
-      if (this.el.hasAttribute('data-icons')) {
-        this.icons = this.el.dataset.icons.split(',');
-        this.addIcons();
-      }
+      this.addIcons();
     }
   }
 
-  addIcons(){
-    this.icons.forEach(icon => {
-      if (icon == 'linkedin'){
-        faLibrary.add(faLinkedin);
-      }
-      if (icon == 'facebook'){
-        faLibrary.add(faFacebookSquare);
-      }
-      if (icon == 'youtube'){
-        faLibrary.add(faYoutubeSquare);
-      }
-      if (icon == 'instagram'){
-        faLibrary.add(faInstagramSquare);
-      }
-    });
+  addIcons() {
+    faLibrary.add(faLinkedin, faFacebookSquare, faYoutubeSquare, faInstagramSquare);
     faDom.i2svg();
   }
 }
