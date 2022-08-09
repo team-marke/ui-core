25659a16107c878927cde06fda9926ae62e6ea1b
marke-tiago-goncalves <tiago@marke.com.br>
2022-08-09 15:54:58 -0300

    Tab e maps

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,2 +1,10 @@
 import 'regenerator-runtime/runtime.js';
 import '../scss/main.scss';
+
+const loadTabs = async () => {
+  if (document.querySelector('button[data-bs-toggle="tab"]')) {
+    await import("bootstrap/js/dist/tab")
+  }
+};
+
+loadTabs();
