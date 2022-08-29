8bc7a12c574d6b98ddba49116917f8f0662f649a
marke-franco-nienow <franco@marke.com.br>
2022-08-29 15:27:58 -0300

    Mais ajustes masthead

--- src/components/MastheadSlider/MastheadSlider.js
+++ src/components/MastheadSlider/MastheadSlider.js
@@ -4,18 +4,22 @@ import Swiper, { Navigation, Pagination, EffectFade } from 'swiper';
   Array.from(document.querySelectorAll('.masthead-slider')).forEach((masthead) => {
     Swiper.use([Navigation, Pagination, EffectFade]);
     new Swiper(masthead, {
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
       effect: 'fade',
       fadeEffect: { crossFade: true },
       autoHeight: true,
       slidesPerView: 1,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      breakpoints: {
+        992: {
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+        },
+      },
     });
   });
 })();
