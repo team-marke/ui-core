6f03b3a33b48bc6b6bf515c0b8970f8a849ae644
marke-franco-nienow <franco@marke.com.br>
2023-03-13 16:19:50 -0300

    getDataset on select

--- src/components/Form/fields/Select.mk.js
+++ src/components/Form/fields/Select.mk.js
@@ -7,9 +7,20 @@ function Select(content, { label, options, id, required }) {
     return str;
   }
 
+  function getDataset() {
+    if (typeof dataset !== 'object') {
+      return '';
+    }
+    let str = '';
+    for (const key in dataset) {
+      str += `data-${key}="${dataset[key]}"`;
+    }
+    return str;
+  }
+
   return `
     <label for="${id}" class="form-label">${label}</label>
-    <select class="form-select" id="${id}" ${required ? 'required' : ''}>
+    <select class="form-select" id="${id}" ${required ? 'required' : ''} ${getDataset()}>
       ${getOptions()}
     </select>
   `;
