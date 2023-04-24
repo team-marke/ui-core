c4d9b7e7ecc520246f3374bd0624dcbda8e05074
marke-franco-nienow <franco@marke.com.br>
2023-04-24 11:51:37 -0300

    Overlay no masthead

--- src/components/Masthead/Masthead.mk.js
+++ src/components/Masthead/Masthead.mk.js
@@ -1,12 +1,18 @@
 const CloudinaryImgUrl = require('../../core-components/CloudinaryImgUrl');
 
-function Masthead(content, { image, imageMobile }) {
-
+function Masthead(content, { image, imageMobile, overlay }) {
   const defaultTransforms = {
     fetch_format: 'auto',
     quality: 'auto',
     crop: 'thumb',
   };
+
+  function getOverlay() {
+    if (!overlay) return '';
+    return `
+      <div class="masthead__overlay" style="--masthead-overlay-bg: ${overlay};"></div>
+    `;
+  }
 
   function getExactHeight(ogWidth, ogHeight, customWidth) {
     return Math.round(customWidth / (ogWidth / ogHeight));
@@ -21,9 +27,21 @@ function Masthead(content, { image, imageMobile }) {
         height="${image.height}"
         src="${CloudinaryImgUrl(image.src, { ...defaultTransforms, width: image.width, height: image.height })}"
         srcset="
-          ${CloudinaryImgUrl(image.src, { ...defaultTransforms, width: 1440, height: getExactHeight(image.width, image.height, 1440) })} 1440w,
-          ${CloudinaryImgUrl(image.src, { ...defaultTransforms, width: 1920, height: getExactHeight(image.width, image.height, 1920) })} 1920w,
-          ${CloudinaryImgUrl(image.src, { ...defaultTransforms, width: 2560, height: getExactHeight(image.width, image.height, 2560) })} 2560w
+          ${CloudinaryImgUrl(image.src, {
+            ...defaultTransforms,
+            width: 1440,
+            height: getExactHeight(image.width, image.height, 1440),
+          })} 1440w,
+          ${CloudinaryImgUrl(image.src, {
+            ...defaultTransforms,
+            width: 1920,
+            height: getExactHeight(image.width, image.height, 1920),
+          })} 1920w,
+          ${CloudinaryImgUrl(image.src, {
+            ...defaultTransforms,
+            width: 2560,
+            height: getExactHeight(image.width, image.height, 2560),
+          })} 2560w
         "
       >
     `;
@@ -36,11 +54,27 @@ function Masthead(content, { image, imageMobile }) {
         alt="${imageMobile.alt}"
         width="${imageMobile.width}"
         height="${imageMobile.height}"
-        src="${CloudinaryImgUrl(imageMobile.src, { ...defaultTransforms, width: imageMobile.width, height: imageMobile.height })}"
+        src="${CloudinaryImgUrl(imageMobile.src, {
+          ...defaultTransforms,
+          width: imageMobile.width,
+          height: imageMobile.height,
+        })}"
         srcset="
-          ${CloudinaryImgUrl(imageMobile.src, { ...defaultTransforms, width: 375, height: getExactHeight(imageMobile.width, imageMobile.height, 375) })} 375w,
-          ${CloudinaryImgUrl(imageMobile.src, { ...defaultTransforms, width: 425, height: getExactHeight(imageMobile.width, imageMobile.height, 425) })} 425w,
-          ${CloudinaryImgUrl(imageMobile.src, { ...defaultTransforms, width: 768, height: getExactHeight(imageMobile.width, imageMobile.height, 768) })} 768w
+          ${CloudinaryImgUrl(imageMobile.src, {
+            ...defaultTransforms,
+            width: 375,
+            height: getExactHeight(imageMobile.width, imageMobile.height, 375),
+          })} 375w,
+          ${CloudinaryImgUrl(imageMobile.src, {
+            ...defaultTransforms,
+            width: 425,
+            height: getExactHeight(imageMobile.width, imageMobile.height, 425),
+          })} 425w,
+          ${CloudinaryImgUrl(imageMobile.src, {
+            ...defaultTransforms,
+            width: 768,
+            height: getExactHeight(imageMobile.width, imageMobile.height, 768),
+          })} 768w
         "
       >
     `;
@@ -48,11 +82,12 @@ function Masthead(content, { image, imageMobile }) {
 
   return `
     <div class="masthead">
-      ${getImage()}
-      ${getImageMobile()}
-      <div class="masthead__content">
-        ${content}
-      </div>
+    ${getImage()}
+    ${getImageMobile()}
+    ${getOverlay()}
+    <div class="masthead__content">
+    ${content}
+    </div>
     </div>
   `;
 }
