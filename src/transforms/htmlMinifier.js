a1f0fdeb497cda1560388d15fb99abba2bde7c5b
marke-franco-nienow <franco@marke.com.br>
2023-08-18 10:13:35 -0300

    Minify JS e CSS no htmlMinifier

--- src/transforms/htmlMinifier.js
+++ src/transforms/htmlMinifier.js
@@ -1,11 +1,13 @@
 const htmlmin = require('html-minifier');
 
 module.exports = function (content, outputPath) {
-  if (outputPath && outputPath.endsWith(".html")) {
+  if (outputPath && outputPath.endsWith('.html')) {
     let minified = htmlmin.minify(content, {
       useShortDoctype: true,
       removeComments: true,
-      collapseWhitespace: true
+      collapseWhitespace: true,
+      minifyJS: true,
+      minifyCSS: true,
     });
     return minified;
   }
