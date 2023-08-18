3ff62d733dfe59ec49ae75aff6de1129881797c3
marke-franco-nienow <franco@marke.com.br>
2023-08-18 10:17:06 -0300

    Revert "Minify JS e CSS no htmlMinifier"
    This reverts commit a1f0fdeb497cda1560388d15fb99abba2bde7c5b.

--- src/transforms/htmlMinifier.js
+++ src/transforms/htmlMinifier.js
@@ -1,13 +1,11 @@
 const htmlmin = require('html-minifier');
 
 module.exports = function (content, outputPath) {
-  if (outputPath && outputPath.endsWith('.html')) {
+  if (outputPath && outputPath.endsWith(".html")) {
     let minified = htmlmin.minify(content, {
       useShortDoctype: true,
       removeComments: true,
-      collapseWhitespace: true,
-      minifyJS: true,
-      minifyCSS: true,
+      collapseWhitespace: true
     });
     return minified;
   }
