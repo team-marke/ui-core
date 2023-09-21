0ca3e76a7d083c42576227976d6a07fa6d4c52ba
marke-franco-nienow <franco@marke.com.br>
2023-09-21 15:20:46 -0300

    Form gsheets

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,11 +1,14 @@
 import Form from './js/form';
 import HubspotForm from './js/extensions/hubspot-form';
 import MailchimpForm from './js/extensions/mailchimp-form';
+import GsheetsSaveForm from './js/extensions/gsheets-save-form';
 
 (() => {
   Array.from(document.querySelectorAll('.form')).forEach((form) => {
     if (form.dataset.integration) {
       switch (form.dataset.integration) {
+        case 'gsheets':
+          return new GsheetsSaveForm(form);
         case 'hubspot':
           return new HubspotForm(form);
         case 'mailchimp':
---
+++ src/components/Form/js/extensions/gsheets-save-form.js
@@ -0,0 +1,50 @@
+import Form from '../form';
+
+/**
+ * Form for saving the data in to a Google Sheet
+ * @param {HTMLFormElement} - The form element itself
+ */
+export default class GsheetsSaveForm extends Form {
+  constructor(form) {
+    super(form);
+    this.fileId = form.dataset.fileId;
+  }
+
+  getDate() {
+    return new Date().toLocaleDateString();
+  }
+
+  getFormData() {
+    let data = {};
+    this.fields.forEach((field) => {
+      if (!field.value) return;
+      data[field.dataset.gsheetField] = field.value;
+    });
+    return data;
+  }
+
+  async submit() {
+    this.btnSpinner.startSpin();
+    try {
+      const url = `https://api.apispreadsheets.com/data/${this.fileId}`;
+      const headers = { 'Content-Type': 'application/json' };
+      const body = JSON.stringify({
+        data: this.getFormData(),
+      });
+      const res = await fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: body,
+      });
+      if (res.status == 201) {
+        this.showFeedback(this.successMsg, 'success');
+        this.redirectURL();
+      } else {
+        this.showFeedback(this.errorMsg, 'danger');
+      }
+    } catch (error) {
+      this.showFeedback(this.errorMsg, 'danger');
+    }
+    this.btnSpinner.stopSpin();
+  }
+}
